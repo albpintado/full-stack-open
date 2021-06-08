76a5b9e960ea98546b33b39bd5cdb06885d09e2b
Alberto Pintado <albertopintadolozano@gmail.com>
2021-06-08 09:52:42 +0200

    Step4, exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -14,21 +14,28 @@ const Persons = ({ persons }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "691821038" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [matchedPersons, setMatchedPersons] = useState(persons);
 
   const addPerson = (event) => {
     event.preventDefault();
-    console.log();
     if (isNameAlreadyAdded()) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
     setPersons(persons.concat({ name: newName, number: newNumber }));
+    setMatchedPersons(
+      matchedPersons.concat({ name: newName, number: newNumber })
+    );
     setNewName("");
     setNewNumber("");
+    console.log(persons);
   };
 
   const isNameAlreadyAdded = () => {
@@ -38,9 +45,20 @@ const App = () => {
   const nameHandler = (event) => setNewName(event.target.value);
   const numberHandler = (event) => setNewNumber(event.target.value);
 
+  const personsHandler = (event) => {
+    const personsFiltered = persons.filter(
+      (person) =>
+        person.name.toLowerCase().indexOf(event.target.value.toLowerCase()) >= 0
+    );
+    setMatchedPersons(personsFiltered);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <label htmlFor="search">Search name </label>
+      <input name="search" onChange={personsHandler} />
+      <h2>Add a new one</h2>
       <form>
         <label htmlFor="name">Name </label>
         <input name="name" onChange={nameHandler} value={newName} />
@@ -51,7 +69,7 @@ const App = () => {
         <button onClick={addPerson}>Add</button>
       </form>
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <Persons persons={matchedPersons} />
     </div>
   );
 };

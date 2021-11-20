5e78e00bede3be6d32ba2a8232bbe1fd7e4fb909
Alberto Pintado <albertopintadolozano@gmail.com>
2021-11-20 15:16:10 +0100

    4.4: helper functions and unit tests, step2

--- part4/src/utils/list_helper.js
+++ part4/src/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const numberOfTotalLikes = blogs.reduce((acc, blog) => acc + blog.likes, 0);
+  return numberOfTotalLikes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };

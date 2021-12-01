d0965a5dce1132cafb9708294ceca7277c43be72
Alberto Pintado <albertopintadolozano@gmail.com>
2021-12-01 19:59:25 +0100

    Set test environment

--- part4/src/utils/config.js
+++ part4/src/utils/config.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 
-const { MONGO_URL } = process.env;
+const MONGO_URL =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
+
 const { PORT } = process.env;
 
 module.exports = { PORT, MONGO_URL };
